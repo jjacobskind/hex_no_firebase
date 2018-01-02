17453512dcef657e6e948ec2cbd52653049cf1fe
jjacobskind <jeremy.jacobskind@gmail.com>
2018-01-01 23:02:30 -0500

    Fixes syntax bug

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,35 +3,35 @@ import {renderToStaticMarkup} from 'react-dom/server'
 // import MainMenu from './reactTemplates/MainMenu'
 
 export default function ($scope, $state, $http, $window, Auth, authFactory, boardFactory, engineFactory, socket, $q, $rootScope, $sce) {
-    var self = this;
-    self.player_name;
-    var testcanvas = document.createElement('canvas');
-    self.meets_reqs = !!(testcanvas.getContext("webgl") || testcanvas.getContext("experimental-webgl"));
-    self.player_name = authFactory.getPlayerName() || 'J';
+  var self = this;
+  self.player_name;
+  var testcanvas = document.createElement('canvas');
+  self.meets_reqs = !!(testcanvas.getContext("webgl") || testcanvas.getContext("experimental-webgl"));
+  self.player_name = authFactory.getPlayerName() || 'J';
 
-    self.previousGameIDs = [];
+  self.previousGameIDs = [];
 
-    self.createNewGame = function(board_size) {
-        $state.go('game', { task: 'create', size: board_size });
-    };
+  self.createNewGame = function(board_size) {
+      $state.go('game', { task: 'create', size: board_size });
+  };
 
-    self.joinGame = function(gameID){
-        $state.go('game', { id: gameID, task: 'join' });
-    };
+  self.joinGame = function(gameID){
+      $state.go('game', { id: gameID, task: 'join' });
+  };
 
-    self.loadUsersGameList = function(){
-        $http.get('/api/users/' + Auth.getCurrentUser()._id + '/games')
-            .success(function(data){
-                self.previousGameIDs = data.gamesList;
-                $state.go('main.load');
-            });
-    }
+  self.loadUsersGameList = function(){
+      $http.get('/api/users/' + Auth.getCurrentUser()._id + '/games')
+          .success(function(data){
+              self.previousGameIDs = data.gamesList;
+              $state.go('main.load');
+          });
+  }
 
-    self.loadPreviousGame = function(gameID) {
-        $state.go('game', { id: gameID, task: 'load' });
-    };
+  self.loadPreviousGame = function(gameID) {
+      $state.go('game', { id: gameID, task: 'load' });
+  };
 
-    self.loginOauth = function(provider) {
-        $window.location.href = '/auth/' + provider;
-    };
-  });
+  self.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+  };
+}
